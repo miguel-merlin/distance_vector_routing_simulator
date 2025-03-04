e7b19f9a1a13359f71ce55f732f708a696c041f0
Butter - ThinkWin <butterman423@gmail.com>
2025-03-04 15:47:24 -0500

    Added Entity Class and applied Mapping changes

--- src/frontend/components/entity.ts
+++ src/frontend/components/entity.ts
@@ -5,8 +5,30 @@ export type uid = number
 export interface BaseEntity {
     id: uid
     name: string
+    type: undefined
 }
 
-export type EntityMap = Record<uid, any>
+export class Entity {
+    ent: unknown
+
+    static of<T extends BaseEntity>(ent: T): Entity {
+        return new Entity(ent)
+    }
+
+    /** Use static method of<T>() for better type safety */
+    constructor(e: any) {
+        this.ent = e
+    }
+
+    is(t: symbol): boolean {
+        return (this.ent as BaseEntity).type === t
+    }
+
+    getAs<T extends BaseEntity>(): T {
+        return (this.ent as T)
+    }
+}
+
+export type EntityMap = Record<uid, Entity>
 
 export const EntityContext: React.Context<EntityMap> = createContext({})
