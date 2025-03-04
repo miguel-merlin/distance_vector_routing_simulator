1536811abf23fd8dd8e15f444a36ceafd33897b5
Butter - ThinkWin <butterman423@gmail.com>
2025-03-04 17:24:49 -0500

    Rewrote <Simulator> to conform to Entity changes

--- src/frontend/Simulator.tsx
+++ src/frontend/Simulator.tsx
@@ -1,6 +1,6 @@
-import { EntityMap, EntityContext } from "./components/entity"
-import { Node } from "./components/Node"
-import { Edge } from "./components/Edge"
+import { type EntityMap, EntityContext, Entity } from "./components/entity"
+import { Node, NodeEntity } from "./components/Node"
+import { Edge, EdgeEntity } from "./components/Edge"
 import { Stage, Layer } from 'react-konva'
 
 interface Simulator {
@@ -8,20 +8,20 @@ interface Simulator {
 }
 
 export function Simulator(_props: Simulator) {
-    const env: EntityMap = {
-        1: { id: 1, name: 'A', x: 50, y: 50 },
-        2: { id: 2, name: 'B', x: 300, y: 100 },
-        3: { id: 3, name: 'e1', head: 1, tail: 2}
-    }
+   const env: EntityMap = {
+        1: Entity.of<NodeEntity>({ id: 1, name: 'A', x: 50, y: 50 }),
+        2: Entity.of<NodeEntity>({ id: 2, name: 'B', x: 300, y: 100 }),
+        3: Entity.of<EdgeEntity>({ id: 3, name: 'e1', head: 1, tail: 2 })
+   }
     return (
         <Stage width={500} height={500}>
             <EntityContext value={env}>
                 <Layer>
-                    <Edge {...env[3]}/>
+                    <Edge ent={env[3]}/>
                 </Layer>
                 <Layer>
-                    <Node {...env[1]}/>
-                    <Node {...env[2]}/>
+                    <Node ent={env[1]}/>
+                    <Node ent={env[2]}/>
                 </Layer>
             </EntityContext>
         </Stage>
