c5061912367a78c14f680948ccc016f3ea0667aa
Butter - ThinkWin <butterman423@gmail.com>
2025-03-18 19:42:12 -0400

    Added Attribute Objects with Defaults

---
+++ src/frontend/components/_defaults.ts
@@ -0,0 +1,13 @@
+import type { DecorateAttr, LabelAttr } from "./attributes";
+
+export const DECOR_DEFAULTS: DecorateAttr = {
+    fill: 'white',
+    stroke: 'black',
+    strokeWeight: 2.5,
+    highlightColor: undefined,
+}
+
+export const LABEL_DEFAULTS: LabelAttr = {
+    label: 'Sample Text',
+    labelClr: 'black'
+}
