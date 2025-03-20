257f5c72d4e419ea0ed6f137985883ffdff2b354
Butter - ThinkWin <butterman423@gmail.com>
2025-03-19 22:06:04 -0400

    Attached LabelAttr to Node

--- src/frontend/components/Node.tsx
+++ src/frontend/components/Node.tsx
@@ -1,6 +1,7 @@
 import { type BaseEntity, type EntityProp } from "./entity"
-import { type PositionAttr, type DecorateAttr, type BaseAttr } from "./attributes"
+import { type PositionAttr, type DecorateAttr, type BaseAttr, LabelAttr } from "./attributes"
 import { Circle } from "react-konva"
+import { DECOR_DEFAULTS, LABEL_DEFAULTS } from "./_defaults"
 
 export const ET_NODE = Symbol("Key that denotes a Node Entity")
 
@@ -12,8 +13,12 @@ export type NodeEntity = BaseEntity
     & NodeAttr
     & PositionAttr
     & Partial<DecorateAttr>
+    & LabelAttr
 
 export function Node({ ent }: EntityProp) {
     const { x, y } = ent.getAttr<PositionAttr>()
+    const label = ent.getAttr<LabelAttr>(LABEL_DEFAULTS);
+    const decor = ent.getAttr<DecorateAttr>(DECOR_DEFAULTS);
+
     return <Circle x={x} y={y} radius={25} fill='green'/>
 }
