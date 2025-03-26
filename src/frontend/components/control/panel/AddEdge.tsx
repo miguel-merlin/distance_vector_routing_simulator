a5364d8996cc3861302cd8ffa8a82b0061dbca15
Nathaniel Escaro <butterman423@gmail.com>
2025-03-26 00:20:03 -0400

    fix: added missing classes for panel headers

--- src/frontend/components/control/panel/AddEdge.tsx
+++ src/frontend/components/control/panel/AddEdge.tsx
@@ -3,7 +3,7 @@ import PanelWrapper from "./util/PanelWrapper";
 export default function AddEdge() {
     return (
         <PanelWrapper>
-            <h1>Add Edge</h1>
+            <h1 className="text-center underline decoration-solid font-bold">Add Edge</h1>
             <label>Id</label>
             <input/>
             <label>Head Id</label>
--- src/frontend/components/control/panel/AddNode.tsx
+++ src/frontend/components/control/panel/AddNode.tsx
@@ -3,7 +3,7 @@ import PanelWrapper from "./util/PanelWrapper";
 export default function AddNode() {
     return (
         <PanelWrapper>
-            <h1>Add Node</h1>
+            <h1 className="text-center underline decoration-solid font-bold">Add Node</h1>
             <label>Id</label>
             <input/>
             <label>Name</label>
