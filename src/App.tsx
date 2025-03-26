f805c127e36948daafdb69a3f109dccef5876783
Butter - ThinkWin <butterman423@gmail.com>
2025-03-26 15:24:18 -0400

    Changed Simulator to accept external env

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Simulator } from "./frontend/Simulator"
 import ControlUI from "./frontend/ControlUI"
+import { useState } from "react"
+import { EntityMap } from "./frontend/util/entity"
+
 function App() {
+  const [entMap, setEntMap] = useState<EntityMap>(new Map())  
+
   return (
     <div className="w-screen h-screen grid content-center place-content-center">
       <div className="flex border-2 w-[750px] h-min">
         <div className="border-r-2">
-          <Simulator/>
+          <Simulator env={entMap}/>
         </div>
         <div className="w-[250px] h-[500px]">
           <ControlUI/>
