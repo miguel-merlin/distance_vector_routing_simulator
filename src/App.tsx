993d53b5e128869f9112e9d8eccee6e932854f6b
Nathaniel Escaro <butterman423@gmail.com>
2025-03-26 00:57:43 -0400

    Centered UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Simulator } from "./frontend/Simulator"
 import ControlUI from "./frontend/ControlUI"
 function App() {
   return (
-    <div className="flex border-2 w-[750px]">
-      <div className="border-r-2">
-        <Simulator/>
-      </div>
-      <div className="w-[250px] h-[500px]">
-        <ControlUI/>
+    <div className="w-screen h-screen grid content-center place-content-center">
+      <div className="flex border-2 w-[750px] h-min">
+        <div className="border-r-2">
+          <Simulator/>
+        </div>
+        <div className="w-[250px] h-[500px]">
+          <ControlUI/>
+        </div>
       </div>
     </div>
   )
