22b7c02308364f1b5429d0f70af225804c88294e
Nathaniel Escaro <butterman423@gmail.com>
2025-03-27 16:43:13 -0400

    Mapped EntityMap to Network

--- src/frontend/Simulator.tsx
+++ src/frontend/Simulator.tsx
@@ -1,27 +1,43 @@
 import { Stage, Layer } from 'react-konva'
-import { type EntityMap, EntityContext } from "+/util/entity"
+import { type EntityMap, Entity, EntityContext } from "+/util/entity"
 import Node from "./components/entity/Node"
-import Edge from "./components/entity/Edge"
+import Edge, { EdgeEntity } from "./components/entity/Edge"
+import { useMemo } from 'react'
+import Network from '+/interfaces/Network'
 
 interface SimulatorProps {
     env: EntityMap
 }
 
 export function Simulator({ env }: SimulatorProps) {
-    const ents = Array.from(env.values())
+    const { layers, network } = useMemo(() => {
+        const sortedEnts: { node: Entity[], links: Entity[] } = { node: [], links: [] }
+        const network = new Network()
+        for(const ent of env.values()) {
+            if(ent.is("ET_NODE"))
+                sortedEnts.node.push(ent)
+            else if(ent.is("ET_EDGE"))
+                sortedEnts.links.push(ent)
+        }
+
+        for(const ent of sortedEnts.node)
+            network.addNode(ent.getAs().id)
+        for(const ent of sortedEnts.links) {
+            const eEnt = ent.getAs<EdgeEntity>()
+            network.addLink(eEnt.head, eEnt.tail, eEnt.weight)
+        }
+
+        return { layers: sortedEnts, network }
+    }, [env])
 
     return (
         <Stage width={500} height={500}>
             <EntityContext value={env}>
                 <Layer>
-                    { ents.filter((e) => e.is("ET_EDGE")) 
-                        .map((e, idx) => <Edge key={idx} ent={e}/>)
-                    }
+                    { layers.links.map((e, idx) => <Edge key={idx} ent={e}/>) }
                 </Layer>
                 <Layer>
-                    { ents.filter((e) => e.is("ET_NODE"))
-                        .map((e, idx) => <Node key={idx} ent={e}/>)
-                    }
+                    { layers.node.map((e, idx) => <Node key={idx} ent={e}/>) }
                 </Layer>
             </EntityContext>
         </Stage>
