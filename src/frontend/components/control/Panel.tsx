c5ceb10ef64c23ad00503ea9e3e188f05060cfba
Nathaniel Escaro <butterman423@gmail.com>
2025-04-17 01:46:26 -0400

    Extended FieldType and InputContainer values

--- src/frontend/components/control/Panel.tsx
+++ src/frontend/components/control/Panel.tsx
@@ -1,13 +1,32 @@
 import Field, { RawInputContainer } from "./util/Field"
 import { RRefHook } from "+/util/react-aliases"
+import { Vector2d } from "konva/lib/types"
 
-type FieldConf = { key: string, label?: string, type: "number" | "string" }
-type InputContainer = Record<string, number | string>
+type FieldType = "number" | "string" | "vector" | "id"
+type FieldValue = number | string | Vector2d
+type FieldConf = { key: string, label?: string, type: FieldType }
+type InputContainer = Record<string, FieldValue>
 
 export interface PanelProps {
     fields: FieldConf[]
     inputs: RRefHook<RawInputContainer>
     onSubmit: (f: InputContainer) => void
+}
+
+function parseFieldValue(type: FieldType, rawVal: string) : FieldValue {
+    switch(type) {
+        case "number":
+            return Number.parseInt(rawVal)
+        case "vector":
+            const coords = rawVal.match(/\d+/g)
+            if(!coords || coords.length !== 2) throw new Error(`Malformed vector given ${rawVal}`)
+            return { x: Number.parseInt(coords[0]), y: Number.parseInt(coords[1]) }
+        case "string":
+        case "id":
+            return rawVal
+        default:
+            throw new Error(`Unknown fieldType provided ${type}`)
+    }
 }
 
 export default function Panel({ inputs, fields, onSubmit }: PanelProps) {
@@ -21,8 +40,7 @@ export default function Panel({ inputs, fields, onSubmit }: PanelProps) {
                         const rawInps = inputs.current
                         const rawV = rawInps[key]
                         if(!rawV) return {}
-                        const v = type === "number" ? Number.parseInt(rawV) : rawV
-                        return { [key]: v }
+                        return { [key]: parseFieldValue(type, rawV) }
                     })
                     .reduce((p, c) => ({...p, ...c}))
                 onSubmit(inp)
