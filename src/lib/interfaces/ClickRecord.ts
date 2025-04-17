ab78cd572d5c6a0df45dde7011484d29d6909dc5
Nathaniel Escaro <butterman423@gmail.com>
2025-04-17 00:23:06 -0400

    Implemented Event driven ClickRecord functionality

--- src/lib/interfaces/ClickRecord.ts
+++ src/lib/interfaces/ClickRecord.ts
@@ -1,11 +1,18 @@
 import { Entity } from "+/util/entity";
 import { Vector2d } from "konva/lib/types";
 
+const CLICK_EVENT_ID = "SIM_CLICK"
+
 export class ClickRecord {
+    private _clickEvent: CustomEvent
+    private _cancelController: AbortController
     target: Entity | null
     pos: Vector2d | null
 
     constructor() {
+        this._clickEvent = new CustomEvent(CLICK_EVENT_ID)
+        this._cancelController = new AbortController()
+
         this.target = null
         this.pos = null
     }
@@ -24,5 +31,34 @@ export class ClickRecord {
 
     setTarget(ent: Entity) {
         this.target = ent
+        
+    }
+
+    fireUpdate() {
+        dispatchEvent(this._clickEvent)
+    }
+
+    cancel() {
+        this._cancelController.abort()
+    }
+
+    async waitForUpdate() : Promise<{ target: Entity | null, pos: Vector2d | null }> {
+        return new Promise((resolve, reject) => {
+            const cleanupController = new AbortController()
+            const canceled = this._cancelController.signal
+            const cleanup = cleanupController.signal
+
+            const finished = AbortSignal.any([canceled, cleanup])
+
+            addEventListener(CLICK_EVENT_ID, () => {
+                cleanupController.abort()
+                resolve({ target: this.target, pos: this.pos })
+            }, { signal: finished })
+
+            canceled.addEventListener("abort", () => {
+                cleanupController.abort()
+                reject("Canceled by user")
+            }, { signal: finished })
+        })
     }
 }
