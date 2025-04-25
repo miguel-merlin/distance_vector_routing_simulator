5477a73790ef7d4f3ee25a250b6c01737948ca03
Nathaniel Escaro <butterman423@gmail.com>
2025-04-25 02:17:56 -0400

    fix: Removed references to deleted lib/util/sim-event

--- src/frontend/components/entity/util/Environment.tsx
+++ src/frontend/components/entity/util/Environment.tsx
@@ -1,7 +1,6 @@
-import { EntityContext, TimeContext, EventContext} from "+/util/contexts";
-import { EventQueue } from "+/util/sim-event";
+import { EntityContext, TimeContext } from "+/util/contexts";
 import { EntityMap } from "+/util/entity";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const interval = 1000
 
@@ -13,7 +12,6 @@ export interface EnvironmentProps {
 
 export default function Environment({ paused, entMap, children }: EnvironmentProps) {
     const [t, setT] = useState(0)
-    const eventRefs = useRef<EventQueue>([])
 
     useEffect(() => {
         if(paused) return
@@ -27,9 +25,7 @@ export default function Environment({ paused, entMap, children }: EnvironmentPro
     return (
         <EntityContext value={entMap}>
             <TimeContext value={t}>
-                <EventContext value={eventRefs.current}>
-                    { children }
-                </EventContext>
+                { children }
             </TimeContext>
         </EntityContext>
     )
--- src/lib/util/contexts.ts
+++ src/lib/util/contexts.ts
@@ -1,12 +1,10 @@
 import { EntityMap } from "./entity"
 import { createContext } from "react"
 import { RContext } from "./react-aliases"
-import { EventQueue } from "./sim-event"
 import { ClickRecord } from "+/interfaces/ClickRecord"
 import { PacketController } from "+/interfaces/PacketController"
 
 export const EntityContext: RContext<EntityMap> = createContext(new Map())
 export const TimeContext: RContext<number> = createContext(0)
-export const EventContext: RContext<EventQueue> = createContext<EventQueue>([])
 export const ClickContext: RContext<ClickRecord> = createContext<ClickRecord>(new ClickRecord())
 export const PacketContext: RContext<PacketController> = createContext<PacketController>(new PacketController())
