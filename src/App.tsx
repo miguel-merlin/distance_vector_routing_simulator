bc0724b088a0240847edbaeeba81eeae9015748f
Nathaniel Escaro <butterman423@gmail.com>
2025-05-08 01:40:14 -0400

    Removed sample map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { Simulator } from "@/Simulator"
 import ControlUI from "@/ControlUI"
 import { useState } from "react"
-import { Entity, EntityMap } from "+/util/entity"
-import { NodeEntity } from "@/components/entity/Node"
-import { EmitterEntity } from "@/components/entity/Emitter"
-import { EdgeEntity } from "@/components/entity/Edge"
+import { EntityMap } from "+/util/entity"
 import Network from "+/interfaces/Network"
 
 function prettyPrint(str: string) {
@@ -19,13 +16,7 @@ function prettyPrint(str: string) {
 }
 
 function App() {
-  const envState = useState<EntityMap>(new Map([
-    ["A", Entity.of<EmitterEntity>({type: "ET_EMIT", id: "A", name: "A", x: 50, y: 50, size: 50, spawnRate: 5})],
-    ["B", Entity.of<EmitterEntity>({type: "ET_EMIT", id: "B", name: "B", x: 400, y: 400, size: 50, spawnRate: 5, disabled: true})],
-    ["R1", Entity.of<NodeEntity>({type: "ET_NODE", id: "R1", name: "R1", x: 150, y: 250, size: 25})],
-    ["e1", Entity.of<EdgeEntity>({type: "ET_EDGE", id: "e1", name: "e1", head: "A", tail: "R1", weight: 2})],
-    ["e2", Entity.of<EdgeEntity>({type: "ET_EDGE", id: "e1", name: "e1", head: "R1", tail: "B", weight: 6})]
-  ]))
+  const envState = useState<EntityMap>(new Map())
   const pausedState = useState<boolean>(true);
   const networkState = useState<Network | null>(null)
   const [entMap] = envState
